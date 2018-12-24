b77e845595bd515be933f4f89e4f67e2101e0a45
RocketStormNet <rocketstormnet@outlook.com>
2018-12-24 12:21:25 +0500

    Added publication model logic and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+    email: 'rocketstormnet@outlook.com',
+    password: 'password',
+    is_admin: true
+)
